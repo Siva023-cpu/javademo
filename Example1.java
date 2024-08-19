a6762d06c2268186206d969e41c8bf67875385b2
Siva023-cpu <mssk6304445254@gmail.com>
2024-08-19 11:04:32 +0530

    Update Example1.java
    second commit

--- Example1.java
+++ Example1.java
@@ -3,5 +3,7 @@ class Example1
     public static void main(String args[])
     {
       System.out.println("First FreeStyle Project");
+      System.out.println("First FreeStyle Project commit changes");
+
     }
   }
