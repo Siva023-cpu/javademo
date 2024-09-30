f1c381777e1f0f31612f8f7d8d1e7e06925e70c3
Siva023-cpu <mssk6304445254@gmail.com>
2024-09-30 11:06:40 +0530

    Update Example1.java
    update

--- Example1.java
+++ Example1.java
@@ -2,8 +2,8 @@ class Example1
   {
     public static void main(String args[])
     {
-      System.out.println("First FreeStyle Project");
-      System.out.println("First FreeStyle Project commit changes");
+      System.out.println("hello ");
+      System.out.println("new pipeline");
 
     }
   }
